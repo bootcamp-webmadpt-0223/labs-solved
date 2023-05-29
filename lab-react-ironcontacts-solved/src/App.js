8259eeb4280d0b44e9e0ebcd15817e1db312b916
Andrés García <playsharp59@gmail.com>
2023-05-29 19:00:14 +0200

    React ironcontacts solved

---
+++ lab-react-ironcontacts-solved/src/App.js
@@ -0,0 +1,81 @@
+import './App.css'
+import { useState } from 'react'
+import allContacts from './contacts.json'
+
+function App() {
+  // Iteration 1
+  const [visibleContacts, setVisibleContacts] = useState(allContacts.slice(0, 6))
+
+  // Iteration 3
+  const addRandomContact = () => {
+    const remainingContacts = allContacts.filter(contact => {
+      return !visibleContacts.some(visibleContact => visibleContact.id === contact.id)
+    })
+    const randomIndex = Math.floor(Math.random() * remainingContacts.length)
+    const randomContact = remainingContacts[randomIndex]
+    if (remainingContacts.length > 0) {
+      setVisibleContacts([...visibleContacts, randomContact])
+    }
+  }
+
+  // Iteration 4
+  const sortByName = () => {
+    const copy = [...visibleContacts]
+    copy.sort((a, b) => a.name.localeCompare(b.name))
+    setVisibleContacts(copy)
+  }
+  const sortByPopularity = () => {
+    const copy = [...visibleContacts]
+    copy.sort((a, b) => b.popularity - a.popularity)
+    setVisibleContacts(copy)
+  }
+
+  // Iteration 5
+  const deleteContact = deleteThisId => {
+    const filtered = visibleContacts.filter(contact => contact.id !== deleteThisId)
+    return setVisibleContacts(filtered)
+  }
+
+  return (
+    <div className='App'>
+      <h1>IronContacts</h1>
+      <div className='actions'>
+        <button onClick={addRandomContact}>Add Random Contact</button>
+        <button onClick={sortByName}>Sort by Name</button>
+        <button onClick={sortByPopularity}>Sort by Popularity</button>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
+            <th>Remove</th>
+          </tr>
+        </thead>
+        <tbody>
+          {visibleContacts.map(contact => {
+            return (
+              <tr key={contact.id}>
+                <td>
+                  <img src={contact.pictureUrl} alt={contact.name} />
+                </td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity.toFixed(2)}</td>
+                {contact.wonOscar ? <td>🏆</td> : <td></td>}
+                {contact.wonEmmy ? <td>🏆</td> : <td></td>}
+                <td>
+                  <button onClick={() => deleteContact(contact.id)}>❌</button>
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default App
